ce4f17d71899899ff728f90e8001a03a0b539ab8
Bakanet <32592013+Bakanet@users.noreply.github.com>
2017-11-17 12:47:00 +0100

    Add files via upload

--- SUP/C#/TP0/TP0/TP0/Program.cs
+++ SUP/C#/TP0/TP0/TP0/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
-using System.Data;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Xsl.Runtime;
 
 namespace TP0
 {
@@ -10,7 +11,7 @@ namespace TP0
     {
         public static void Main(string[] args)
         {
-           
+            Console.WriteLine(MyGcd(74, 148));
         }
 
         static void HelloWorld()
@@ -39,24 +40,70 @@ namespace TP0
             return x;
         }
 
-        static uint MyFact(uint n)
-        {
-            if (n == 1)
-                return n;
-            return n * MyFact(n - 1);
-        }
-
         static double MyPow(double x, int n)
         {
-            if (n = 0)
+            if (n == 0)
+                return 1;
+            else if (n == 1)
                 return x;
-            return x * MyPow(x, n - 1);
-
+            else if (n < 0)
+                return (1 / x) * MyPow(x, n + 1);
+            else
+                return x * MyPow(x, n - 1);
         }
 
-        static uint MyFibo(uint n);
+        static uint MyFibo(uint n)
         {
-            
+            if (n == 0 || n == 1)
+                return n;
+            else
+                return MyFibo(n - 1) + MyFibo(n - 2);
         }
+
+        static string ChangeChar(string s, char c, uint n)
+        {
+            int l = s.Length;
+            int nth = (int) n;
+            if (n > l)
+                return s;
+            else
+            {
+                return s.Substring(0, nth) + c + s.Substring(nth + 1);
+            }
+
+        }
+
+        static int MyGcd(uint a, uint b)
+        {
+            int a2 = (int) a;
+            int b2 = (int) b;
+
+            if (a2 > b2)
+            {
+                int r = a2 % b2;
+                if (r == 0)
+                    return b2;
+                else
+                {
+                    uint b3 = (uint) b2;
+                    uint r2 = (uint) r;
+                    return MyGcd(b3, r2);
+                }
+            }
+
+            else
+            {
+                int r = b2 % a2;
+                if (r == 0)
+                    return a2;
+                else
+                {
+                    uint a3 = (uint) a2;
+                    uint r2 = (uint) r;
+                    return MyGcd(a3, r2);
+                }
+            }
+        }
+        
     }
 }
