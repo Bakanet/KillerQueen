8f4c47b08dacd36afb8b5877b10270803a9112aa
Bakanet <32592013+Bakanet@users.noreply.github.com>
2017-11-22 16:01:31 +0100

    debut TP

---
+++ SUP/C#/TP1/TP1/TP1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TP1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bernard.");
+            Console.WriteLine(42);
+            Console.Write("Hello");
+            Console.Write("Hello\n");
+            Console.WriteLine("What door ?");
+        }
+    }
+}
