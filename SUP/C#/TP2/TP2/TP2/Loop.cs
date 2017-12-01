0834afa390ed57a99eb2652081dc3f3aa32cc0f4
Bakanet <malo.lecomte77@gmail.com>
2017-12-01 21:08:17 +0100

    début TP2

---
+++ SUP/C#/TP2/TP2/TP2/Loop.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+
+namespace Debugger
+{
+    public static class Loop
+    {
+        public static void Print_Naturals(int n)
+        {
+            if (n <= 0)
+                Console.WriteLine("Print_Naturals: invalid argument");
+            else
+            {
+                for (int i = 1; i < n; i++)
+                    {
+                        Console.Write(i + " ");
+                    }
+                Console.WriteLine(n);
+            }
+        }
+
+        public static void Print_Primes(int n)
+        {
+            // TODO
+        }
+
+        public static long Fibonacci(long n)
+        {
+            // TODO
+            return 0;
+        }
+
+        public static long Factorial(long n)
+        {
+            // TODO
+            return 0;
+        }
+
+        public static void Print_Strong(int n)
+        {
+            // TODO
+        }
+
+        public static float Abs(float n)
+        {
+            // TODO
+            return 0;
+        }
+
+        public static float Sqrt(float n)
+        {
+            // TODO
+            return 0;
+        }
+
+        public static long Power(long a, long b)
+        {
+            // TODO
+            return 0;
+        }
+
+        public static void Print_Tree(int n)
+        {
+            // TODO
+        }
+    }
+}
---
+++ SUP/C#/TP2/TP2/TP2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Debugger
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Loop.Print_Naturals(0);
+        }
+    }
+}
