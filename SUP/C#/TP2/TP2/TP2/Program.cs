ebbb2d01be3b618f2b2c573852aea7b9b1b0e961
Bakanet <32592013+Bakanet@users.noreply.github.com>
2017-12-06 19:05:49 +0100

    presque

---
+++ SUP/C#/TP2/TP2/TP2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using Debugger;
+
+namespace TP2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Loop.Print_Primes(19);
+        }
+    }
+}
