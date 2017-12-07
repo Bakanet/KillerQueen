41704bae5447f1b2dd83602d7f504a8e56e31dfa
Malo Lecomte <malo.lecomte@r02p08.vj305.sm.cri.epita.net>
2017-12-07 10:43:38 +0100

    dossiers Python

--- SUP/Python/TD3/search_char.py
+++ SUP/Python/TD3/search_char.py
@@ -1,7 +1,12 @@
 def search_char(c,s):
-    i = len(s)
-    while c != s[i]:
+    i = len(s) - 1
+    while c != s[i] and i >= 0:
         i -= 1
     return i
 
-print(search_char("h", "chat"))
+def search_char2(c,s):
+    b = -1
+    for i in s:
+        if i == c:
+            b = i
+    return b
