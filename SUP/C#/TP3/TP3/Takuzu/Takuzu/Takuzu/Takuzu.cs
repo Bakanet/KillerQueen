0a268f578973bbbffa5029f1a00cdbee6abcf738
Bakanet <32592013+Bakanet@users.noreply.github.com>
2017-12-08 19:10:45 +0100

    a

--- SUP/C#/TP3/TP3/Takuzu/Takuzu/Takuzu/Takuzu.cs
+++ SUP/C#/TP3/TP3/Takuzu/Takuzu/Takuzu/Takuzu.cs
@@ -7,7 +7,7 @@ namespace Takuzu
     {
         public static void PrintGrid(int[,] grid)
         {
-            int line = grid.GetLength(0);
+            int row = grid.GetLength(0);
             int column = grid.GetLength(1);
             
             Console.Write("   ");                // mettre au bon endroit la premiere ligne
@@ -17,7 +17,7 @@ namespace Takuzu
             }
             Console.WriteLine();
             
-            for (int i = 0; i < line; i++)        // decomposition par lignes
+            for (int i = 0; i < row; i++)       // decomposition par lignes
             {
                 Console.Write(i + " ");
                 
@@ -36,8 +36,16 @@ namespace Takuzu
 
         public static bool IsRowValid(int[,] grid, int row)
         {
-            //FIXME
-            return false;
+            int nb0 = 0, nb1 = 0;
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                if (grid[row, i] == 0)
+                    nb0 += 1;
+                else if (grid[row, i] == 1)
+                    nb1 += 1;
+            }
+            return nb0 == nb1;
         }
         
         public static bool IsColumnValid(int[,] grid, int col)
