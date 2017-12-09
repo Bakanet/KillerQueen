788cd0ecd2db50192fb330da95157c0ec5ebb7bb
Bakanet <malo.lecomte77@gmail.com>
2017-12-09 20:42:00 +0100

    wip

--- SUP/C#/TP3/Basics/Basics/Program.cs
+++ SUP/C#/TP3/Basics/Basics/Program.cs
@@ -7,6 +7,10 @@ namespace Basics
     {
         public static void Main(string[] args)
         {
+            char c = '1';
+            Console.WriteLine(c);
+            Reference.RotChar(ref c, -13);
+            Console.WriteLine(c);
         }
     }
 }
--- SUP/C#/TP3/Basics/Basics/Reference.cs
+++ SUP/C#/TP3/Basics/Basics/Reference.cs
@@ -1,4 +1,6 @@
-﻿namespace Basics
+﻿using System.Security.Cryptography;
+
+namespace Basics
 {
     public class Reference
     {
@@ -19,7 +21,17 @@
 
         public static void RotChar(ref char c, int n)
         {
-            //solut 
+            int c2 = (int) c;
+            if (48 <= c2 || c2 <= 57)
+            {
+                c2 = c2 + n % 10;
+                if (c2 > (int) '9')
+                    c = (char) (((int) '0' - 1) + (c2 - (int) '9'));
+                else if (c2 < (int) '0')
+                    c = (char) (((int) '9' + 1) - ((int) '0' - c2));
+                else
+                    c = (char) c2;
+            }
         }
     }
 }
