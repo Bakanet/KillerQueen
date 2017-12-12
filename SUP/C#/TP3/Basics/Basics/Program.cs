8a6fe4895d97431850d78f2163f060a8087d9576
Bakanet <malo.lecomte77@gmail.com>
2017-12-12 23:40:16 +0100

    FIN TP3

--- SUP/C#/TP3/Basics/Basics/Program.cs
+++ SUP/C#/TP3/Basics/Basics/Program.cs
@@ -8,5 +8,23 @@ namespace Basics
         public static void Main(string[] args)
         {
         }
+
+        public static int BinarySearch(int[] arr, int x)
+        {
+            int left = arr[0], right = arr[arr.Length - 1], mid = 0;
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+                
+                if (x == mid)
+                    return mid;
+                else if (x < mid)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+
+            return -1; // si l'elem n'est pas dans le tableau
+        }
     }
 }
