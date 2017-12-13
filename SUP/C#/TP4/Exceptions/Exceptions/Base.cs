e040afe41848d689f01c5121cd57960ac2b8bf4b
Bakanet <malo.lecomte77@gmail.com>
2017-12-13 01:45:39 +0100

    début tp4

---
+++ SUP/C#/TP4/Exceptions/Exceptions/Base.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public static class Base
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("n must be positive");
+            
+            else
+            {
+                int a = 1, b = 1;
+                while (n > 1)
+                {
+                    a += b;
+                    b += a;
+                    n -= 2;
+                }
+
+                if (n % 2 == 0)
+                    return a;
+                else
+                    return b;
+            }
+        }
+
+        public static float DegToRad(float angle)
+        {
+            // TODO
+            throw new NotImplementedException("Fix this quickly");
+        }
+        
+        public static float RadToDeg(float angle)
+        {
+            // TODO
+            throw new NotImplementedException("Fix this quickly");
+        }
+        
+        public static float Pow(float n, int p)
+        {
+            // TODO
+            throw new NotImplementedException("Fix this quickly");
+        }
+    }
+}
