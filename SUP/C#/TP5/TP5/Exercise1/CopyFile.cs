3c97e6c1a1954e489c2549adf6b679e96ed38e91
Malo Lecomte <malo.lecomte@r03p09.vj305.sm.cri.epita.net>
2017-12-20 15:52:25 +0100

    IO

---
+++ SUP/C#/TP5/TP5/Exercise1/CopyFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+
+namespace Exercise1
+{
+	internal static class CopyFile
+	{
+		public static void CopyAllFile(string source, string destination)
+		{
+			if (!File.Exists(source))
+				throw new ArgumentException("could not open file: " + source);
+			File.Copy(source, destination, true);
+		}
+
+		/// <summary>
+		///   EXERCISE 1.4:
+		///   <para />
+		///   Read the source file and put half of its content into destination file
+		/// </summary>
+		/// <param name="source">the path to the source file</param>
+		/// <param name="destination">the path to the destination file</param>
+		public static void CopyHalfFile(string source, string destination)
+		{
+			if (!File.Exists(source))
+				throw new ArgumentException("could not open file: " + source);
+			// TODO
+		}
+	}
+}
---
+++ SUP/C#/TP5/TP5/Exercise1/PrintFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise1
+{
+	internal static class PrintFile
+	{
+		public static void PrintAllFile(string path)
+		{
+			if (!File.Exists(path))
+				throw new ArgumentException("could not open file: " + path);
+			Console.WriteLine(File.ReadAllText(path));
+		}
+
+		public static void PrintHalfFile(string path)
+		{
+			if (!File.Exists(path))
+				throw new ArgumentException("could not open file: " + path);
+			string[] file = File.ReadAllLines(path);
+
+			for (int i = 0; i < file.Length; i += 2)
+			{
+				 Console.WriteLine(file[i]);
+			}
+		}
+	}
+}
