b1bf05ed9a9a82e02e9b26ffb7a0632288e9249e
Bakanet <malo.lecomte77@gmail.com>
2017-12-25 17:45:28 +0100

    tp5

---
+++ SUP/C#/TP5/malo.lecomte/TP5/Exercise1/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise1
+{
+	internal static class Test
+	{
+		private static readonly string HomePath = 	   Environment.OSVersion.Platform == PlatformID.Unix 
+													|| Environment.OSVersion.Platform == PlatformID.MacOSX
+													?  Environment.GetEnvironmentVariable("HOME")
+													:  Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+		public static void Main(string[] args)
+		{
+			// TODO: put all your tests here like these examples
+			PrintFile.PrintAllFile ("../../../tests/exo1_test1.txt");
+			PrintFile.PrintHalfFile("../../../tests/exo1_test2.txt");
+			CopyFile.CopyAllFile   ("../../../tests/exo1_test1.txt", "../../../all_file.txt");
+			CopyFile.CopyHalfFile  ("../../../tests/exo1_test2.txt", "../../../half_file.txt");
+			Architecture.Architect (Path.Combine(HomePath, "archi"));
+		}
+	}
+}
