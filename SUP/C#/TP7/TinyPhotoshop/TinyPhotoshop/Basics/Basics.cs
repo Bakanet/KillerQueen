9e1d620bf9783c6a432d293bc4ad172434059271
Bakanet <malo.lecomte77@gmail.com>
2018-01-30 16:57:25 +0100

    TP7

---
+++ SUP/C#/TP7/TinyPhotoshop/TinyPhotoshop/Basics/Basics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+namespace TinyPhotoshop
+{
+    public static class Basics
+    {
+        public static Color Grey(Color color)
+        {
+	        Color grey = new Color();
+        }
+
+        public static Color Binarize(Color color)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Color BinarizeColor(Color color)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Color Negative(Color color)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Color Tinter(Color color, Color tint, int factor)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+
+        public static Image Apply(Bitmap img, Func<Color, Color> func)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+    }
+}
