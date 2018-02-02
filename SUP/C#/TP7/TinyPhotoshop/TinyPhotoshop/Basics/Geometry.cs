43cf237b4ce73f886780d2ca75adca522fe7d578
Bakanet <malo.lecomte77@gmail.com>
2018-02-02 06:48:09 +0100

    TP7

--- SUP/C#/TP7/TinyPhotoshop/TinyPhotoshop/Basics/Geometry.cs
+++ SUP/C#/TP7/TinyPhotoshop/TinyPhotoshop/Basics/Geometry.cs
@@ -17,8 +17,18 @@ namespace TinyPhotoshop
         public static Image Shift(Bitmap img, int x, int y)
         {
 			// double boucle for -> i, j pour recup le pixel  +x, +y pour le decaler dans une new Image
-			throw new NotImplementedException();
-		}
+	        Bitmap shifted = new Bitmap(img.Width, img.Height);
+	        
+	        for (int i = 0; i < img.Width; i++)
+	        {
+		        for (int j = 0; j < img.Height; j++)
+		        {
+			        shifted.SetPixel((i + x) % img.Width, (j + y) % img.Height, img.GetPixel(i, j));
+		        }
+	        }
+
+	        return shifted;
+        }
 
         public static Image SymmetryHorizontal(Bitmap img)
         {
