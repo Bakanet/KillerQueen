4c4a1fa568db3a12c09ba008401ff68accef56bd
Bakanet <malo.lecomte77@gmail.com>
2018-02-20 21:20:52 +0100

    TP9

--- SUP/C#/TP9/malo.lecomte/EpitaSpaceProgram/EpitaSpaceProgram/ACDC/IEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EpitaSpaceProgram.ACDC
-{
-    // Represents an entity of a scene.
-    public interface IEntity : Json.ISerializableList
-    {
-        // Entities must implement an update method, that takes in the time delta since the last update.
-        void Update(double delta);
-    }
-}
