7fcaad4149795d63885420aa355a774b6287330e
Bakanet <malo.lecomte77@gmail.com>
2018-02-20 20:57:00 +0100

    TP algo

--- SUP/Python/TP1/malo.lecomte_heap.py
+++ SUP/Python/TP1/malo.lecomte_heap.py
@@ -56,18 +56,10 @@ def heapPop(H):
     if isEmpty(H):
         raise Exception("Heap is empty")
     length = len(H)
-    H[0], H[length - 1] = H[length - 1], H[0]
-    elt = H.pop()
+    H[1], H[length - 1] = H[length - 1], H[1]
+    elt = H.pop()[1]
     posChild = length - 2
-    posParent = posChild // 2
-    
-    #version non opti (on repete juste la meme chose que dans push donc la complexite est plus grande)
-    #future version : 2i et 2i+1
-
-    while posParent > 0 and H[posChild][0] < H[posParent][0]:
-        H[posChild], H[posParent] = H[posParent], H[posChild]
-        posChild = posParent
-        posParent //= 2
+    posParent = 1
 
     return elt
 
@@ -79,59 +71,34 @@ def isHeap(T):
        :rtype: bool
     
     """
-    length = len(T) / 2 - 1
+    length = len(T) // 2
     i = 1
     while i < length and T[i][0] <= T[2*i][0] and T[i][0] <= T[2*i + 1][0]:
         i += 1
-    return i >= length - 1
+    return i >= length
     
 def heapify(H):
     """ makes H a heap (in place) - returns H modified
     
       :param H: list (a complete tree)
       :rtype: list (a heap)
-    """    
+    """
+    # Heap sort method
     if not isHeap(H):
         length = len(H)
         c = 1
-        while c < length // 2 - 1:
+        while c < length // 2 - 1:                                                  # we need to iterate only on half of the list (2 * i, 2 * i + 1)
             for i in range(1, length):
                 if H[i] != None:
                     minimum = i
                     posLeftChild = 2 * i
                     posRightChild = 2 * i + 1
-                    if posLeftChild < length and H[posLeftChild] < H[minimum]:
+                    if posLeftChild < length and H[posLeftChild][0] < H[minimum][0]:      # search minimum between root and left child
                         minimum = posLeftChild
-                    if posRightChild < length and H[posRightChild] < H[minimum]:
+                    if posRightChild < length and H[posRightChild][0] < H[minimum][0]:    # search minimum between root and right child
                         minimum = posRightChild
-                    if minimum != i:
+                    if minimum != i:                                                # if root is minimum, we don't have to swap
                         H[i], H[minimum] = H[minimum], H[i]
             c += 1
     
     return H
-
-heap = heapPush([None, (2, 'G'), (2, 'I'), (8, 'F'), (5, 'B'), (9, 'J'), (20, 'A'), (10, 'C'), (6, 'H'), (12, 'D'), (15, 'E')], 'lol', 14)
-heap2 = [None, (1,'B'), (3, 'K'), (4, 'N')]
-heap3 = [None, (2, 'G'), (2, 'I'), (8, 'F'), (5, 'B'), (9, 'J'), (20, 'A'), (10, 'C'), (6, 'H'), (12, 'D'), (15, 'E')]
-heap4 = heapPush(heapPush(heapPush(heapPush(heapPush(newHeap(),'A',6),'B',4),'C',5),'D',7),'E',3)
-heap5 = [None, (2, 'I'), (5, 'B'), (8, 'F'), (6, 'H'), (9, 'J'), (20, 'A'), (10, 'C'), (15, 'E'), (12, 'D')]
-heap6 = [None, (2, 'G'), (2, 'I'), (8, 'F'), (5, 'B'), (9, 'J'), (20, 'A'), (10, 'C'), (6, 'H'), (12, 'D'), (15, 'E')]
-heap7 = [None, (1, 'A'), (3, 'B'), (2, 'C')]
-heap8 = [None, (1, 'A'), (3, 'B'), (2, 'C'), (7, 'D'), (6, 'E'), (5, 'F'), (4, 'G')]
-heap9 = [None, (20, 'A'), (5, 'B'), (10, 'C'), (12, 'D'), (15, 'E'), (8, 'F'), (2, 'G'), (6, 'H'), (2, 'I'), (9, 'J')]
-print(isHeap(heap))
-print(heap4)
-print(isHeap(heap4))
-print(isHeap(heap5))
-print(heap6)
-heapPop(heap6)
-print(heap6)
-print(isHeap(heap6))
-isHeap(heap8)
-heapPop(heap8)
-print(heap8)
-print(isHeap(heap8))
-print(heap9)
-heap10 = heapify(heap9)
-print(heap10)
-print(isHeap(heap10))
