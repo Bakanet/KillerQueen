95a346231093aa88dc5214e7c04b17491c3c8aa3
BKN <malo.lecomte@epita.fr>
2018-10-10 15:55:33 +0200

    TP2

--- SPE/C/TP2/pw_02_basics/divisor_sum/divisor_sum.c
+++ SPE/C/TP2/pw_02_basics/divisor_sum/divisor_sum.c
@@ -1,3 +1,16 @@
 unsigned long divisor_sum(unsigned long n) {
+    if (n == 1)
+        return 0;
+
+    unsigned long result = 0;
+    for (unsigned long i = 2; i*i <= n; ++i) {
+        if (n % i == 0) {
+            if (i == n/i)
+                result += i;
+            else
+                result += i + n/i;
+        }
+    }
     
+    return result + 1;
 }
