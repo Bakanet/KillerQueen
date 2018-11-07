8e5f2256deeda1b47aee41387afb72130e0c5d28
BKN <malo.lecomte77@gmail.com>
2018-11-07 14:46:20 +0100

    DM1

--- SPE/Python/DM1/malo.lecomte_prefixtrees.py
+++ SPE/Python/DM1/malo.lecomte_prefixtrees.py
@@ -15,17 +15,17 @@ import prefixtreesexample as pt
 
 ################################################################################
 ## MEASURES
-                        
+
 def countwords(T):
     """ count words in dictionnary T
-    
+
     :param T: The prefix tree
     :rtype: int
     :Example:
     >>> countwords(Tree1)
     11
     """
-    
+
     nb = 1 if T.key[1] else 0
     for child in T.children:
         nb += countwords(child)
@@ -33,26 +33,26 @@ def countwords(T):
 
 def longestwordlength(T):
     """ longest word length
-    
+
     :param T: The prefix tree
     :rtype: int
     :Example:
     >>> longestwordlength(Tree1)
     6
     """
-    
+
     return tree.height(T)
 
 def averagelength(T):
     """ average word length
-    
+
     :param T: The prefix tree
     :rtype: float
     :Example:
     >>> averagelength(Tree1)
     4.636363636363637
     """
-    
+
     (lenw, nbw) = _averagelength(T)
     return  lenw/nbw
 
@@ -67,8 +67,8 @@ def _averagelength(T, lenwords=0):
             lenwsum += lenw
             nbwords += nbw
         return (lenwsum, nbwords)
-    
-print(_averagelength(pt.Tree1)) 
+
+print(_averagelength(pt.Tree1))
 
 ###############################################################################
 ## Researches
@@ -76,7 +76,7 @@ print(_averagelength(pt.Tree1))
 
 def searchword(T, word):
     """ search for a word in dictionary
-    
+
     :param T: The prefix tree
     :param word: The word to search for
     :rtype: bool
@@ -86,7 +86,7 @@ def searchword(T, word):
     >>> searchword(Tree1, "Famous")
     True
     """
-    
+
     # FIXME
     return None
 
@@ -95,33 +95,33 @@ def searchword(T, word):
 
 def wordlist(T):
     """ generate the word list
-    
+
     :param T: The prefix tree
     :rtype: list
     :example:
     >>> print(wordlist(Tree1))
     ['case', 'cast', 'castle', 'circle', 'city', 'come', 'could', 'fame', 'famous', 'fan', 'fancy']
     """
-    
+
    # FIXME
     return None
 
 def longestwords(T):
     """ search for the longest words in dictionary
-    
+
     :param T: The prefix tree
     :rtype: list
     :examples: # order in result does not matter
     >>> longestwords(Tree1)
     ['castle', 'circle', 'famous']
     """
-    
+
     # FIXME
     return None
-    
+
 def completion(T, prefix):
     """ generate the list of words with a common prefix
-    
+
     :param T: The prefix tree
     :param pref: the prefix
     :rtype: list
@@ -133,20 +133,20 @@ def completion(T, prefix):
     >>> completion(Tree1, "what")
     []
     """
-    
+
     # FIXME
     return None
 
 def treetofile(T, filename):
     """ save the dictionary in a file
-    
+
     :param T: The prefix tree
     :param filename: the file name
     :example:
     >>> treeToFile(Tree1, "test.txt")
     # give the same file as "textFiles/wordList1.txt" but in alphabetic order
     """
-    
+
     # FIXME
 
 
@@ -155,19 +155,19 @@ def treetofile(T, filename):
 
 def addword(T, word):
     """ add a word in dictionary
-    
+
     :param T: The prefix tree
     :param word: The word to add
     """
-    
+
     # FIXME
 
 def treefromfile(filename):
     """ build the prefix tree from a file of words
-    
+
     :param filename: The file name
     :rtype: Tree
     """
-    
+
     # FIXME
     return None
